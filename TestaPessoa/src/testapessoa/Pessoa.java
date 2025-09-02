447c47f1e3810ecac03dab1facdd6a77e102364b
rafaelacardoso928 <cardosorafaela.2802@gmail.com>
2025-09-02 15:37:19 -0300

    Add files via upload

---
+++ TestaPessoa/src/testapessoa/Pessoa.java
@@ -0,0 +1,19 @@
+
+package testapessoa;
+public class Pessoa {
+    String nome;
+    int idade;
+    String profissao;
+
+    public Pessoa(String nome, int idade, String profissao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.profissao = profissao;
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Profissão: " + profissao);
+    }
+}
---
+++ TestaPessoa/src/testapessoa/TestaPessoa.java
@@ -0,0 +1,8 @@
+
+package testapessoa;
+public class TestaPessoa {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Ana", 25, "Engenheira");
+        p1.exibirDados();
+    }
+}
