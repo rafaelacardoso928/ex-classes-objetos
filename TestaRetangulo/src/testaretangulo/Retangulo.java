03f7d973137fd2f11bdf90b1e3fda4927ed2367a
rafaelacardoso928 <cardosorafaela.2802@gmail.com>
2025-09-02 15:37:39 -0300

    Add files via upload

---
+++ TestaRetangulo/src/testaretangulo/Retangulo.java
@@ -0,0 +1,19 @@
+
+package testaretangulo;
+public class Retangulo {
+    double largura;
+    double altura;
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+}
---
+++ TestaRetangulo/src/testaretangulo/TestaRetangulo.java
@@ -0,0 +1,10 @@
+
+package testaretangulo;
+public class TestaRetangulo {
+    public static void main(String[] args) {
+        Retangulo r1 = new Retangulo(5, 3);
+        System.out.println("Área: " + r1.calcularArea());
+        System.out.println("Perímetro: " + r1.calcularPerimetro());
+    }
+}
+
